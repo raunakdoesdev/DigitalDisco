3242971b649ca32f5f8d58fdb056dae520dc0308
Raunak Chowdhuri <raunakc@mit.edu>
2021-04-16 22:48:13 -0400

    Can pick songs

--- webapp/app.py
+++ webapp/app.py
@@ -1,15 +1,23 @@
 import streamlit as st
+import os
 
 
 def send_request(message):
     import requests
     url = 'http://608dev-2.net/sandbox/sc/raunakc/final/comm.py'
     x = requests.post(url, data={'message': message})
-    st.write(x.text)
 
 
+songs = os.listdir('../songs')
+songs = {
+    song.split('.')[0]: os.path.join('../songs', song)
+    for song in songs
+}
 st.title('Digital Disco')
-message = st.text_input(label='Message')
 
-if st.button('Send to ESP32'):
-    send_request(message)
+song = st.selectbox(label='Song', options=list(songs.keys()))
+color = st.color_picker(label='Color for Song')
+
+if st.button('Submit'):
+    send_request(color + song)
+    st.audio(songs[song])
