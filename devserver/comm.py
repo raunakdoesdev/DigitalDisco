30b7a3c683774d568577efb9f2250fcf9733e5bb
Alyssa Solomon <amsolomon.ams@gmail.com>
2021-05-04 00:10:21 -0400

    Server responds to request of times from server

--- devserver/comm.py
+++ devserver/comm.py
@@ -1,3 +1,5 @@
+from var.jail.home.team00.final.note_onset import beats
+
 def request_handler(request):
     if request['method'] == 'POST':
         with open('/var/jail/home/team00/final/temp.txt', 'w') as f:
@@ -6,4 +8,11 @@ def request_handler(request):
 
     else:
         with open('/var/jail/home/team00/final/temp.txt', 'r') as f:
-            return f.read()
+            songname = '/var/jail/home/team00/final/' + f.read().split(None, 1)[1] + '.wav'
+            times = beats(songname)
+            #format: len(times), times[0], times[1], ... times[n-1], (note: NO trailing comma)
+            n = len(times)
+            out = str(n) + ','
+            for t in times:
+                out += '{:.4f}'.format(t) + ','
+            return out[:-1]
